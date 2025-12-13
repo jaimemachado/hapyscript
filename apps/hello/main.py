c147e9727911b026a5db08dfc327242bda2bddf8
Jaime Machado <jaime.machado@gmail.com>
2025-12-13 12:29:35 +0100

    Add debug logging for service execution context

--- apps/hello/main.py
+++ apps/hello/main.py
@@ -1,5 +1,9 @@
 from stubs.pyscript_builtins import service
 
+import debugpy, sys, os
+log.info("DEBUGPY: python=%s pid=%s file=%s cwd=%s", sys.executable, os.getpid(), __file__, os.getcwd())
+log.info("DEBUGPY: connected=%s", debugpy.is_client_connected())
+
 
 @service()
 def hello():
