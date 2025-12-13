ba1c42e3edf5d896d592ddd5bc3a2c3f6f5e9321
Jaime Machado <jaime.machado@gmail.com>
2025-12-13 12:34:56 +0100

    a

--- apps/hello/main.py
+++ apps/hello/main.py
@@ -1,13 +1,14 @@
 from stubs.pyscript_builtins import service
 
 import debugpy, sys, os
-log.info("DEBUGPY: python=%s pid=%s file=%s cwd=%s", sys.executable, os.getpid(), __file__, os.getcwd())
+log.info("DEBUGPY: python=%s pid=%s cwd=%s", sys.executable, os.getpid(), os.getcwd())
 log.info("DEBUGPY: connected=%s", debugpy.is_client_connected())
 
 
 @service()
 def hello():
     """A more complex service."""
+    debugpy.breakpoint()
     print("Hello service executed.")
     log.error("Hello service executed.")
     # Your logic here
