cb181c2dc3eb8b4e89f1f79ef44e329365f058db
Jaime Machado <jaime.machado@gmail.com>
2026-06-22 19:48:32 +0200

    Update log message in main service for clarity

--- apps/main.py
+++ apps/main.py
@@ -3,6 +3,6 @@ from stubs.pyscript_builtins import service
 @service()
 def main():
     """A more complex service."""
-    log.info("Main service executed.")
+    log.info("Main service executed test.")
     # Your logic here
     pass
